eb37835675be9ab7ea748321f48aea34eed727e2
wendellalvx <wendellthiago02166@gmail.com>
2026-09-03 20:06:42 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -9,18 +9,22 @@ class ContaBancaria:
     return self.__nome
   def get_saldo((self) -> float:
     return self.__saldo
-  def depositar(self, valor: float) -> tuple[bool, str]:
+  def depositar(self, valor: float):
     if valor <= 0:
-      return False,  "O valor do depósito deve ser maior que zero."
+      print("O valor do depósito deve ser maior que zero.")
+      return
     self.__saldo +=valor
-    return True, f"Depósito de R${valor:.2f} realizado! Novo saldo de: R$ {self.__saldo:.2f}:"
+      print(f"Depósito de R${valor:.2f} realizado! Novo saldo de: R$ {self.__saldo:.2f}:")
+    return
     
-  def sacar(self, valor: float) -> tuple[bool, str]:
+  def sacar(self, valor: float):
     if valor <=0:
-      return False, "O valor do saque deve ser maior que zero."
+      print("O valor do saque deve ser maior que zero.")
+      return
     if valor > self.__saldo:
-      return False, f"Saldo insuficiente para esse saque. Saldo disponivel:R${self.__saldo:.2f}"
+      print(f"Saldo insuficiente para esse saque. Saldo disponivel:R${self.__saldo:.2f}")
+      return
     self.__saldo -= valor 
-      return True, f"Saque de R${valor:.2f} realizado! Novo saldo: R${self.__saldo:.2f}"
-  
+      print(f"Saque de R${valor:.2f} realizado! Novo saldo: R${self.__saldo:.2f}")
+      return
   
